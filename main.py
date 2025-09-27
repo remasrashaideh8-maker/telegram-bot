09937c04e4ccabe7231576a04837b32b6afbc869
remasrashaideh8-maker <remasrashaideh8@gmail.com>
2025-09-27 16:45:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = "8478445732:AAExzCN-vAwxZnnhGfhUjZ5EEZSjFtBYbxc"
-WEBHOOK_PATH = "webhook"
-WEBHOOK_URL = f"https://telegram-bot-3u3r.onrender.com/{WEBHOOK_PATH}"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ البوت يعمل!")
@@ -14,8 +12,7 @@ def main():
     app.run_webhook(
         listen="0.0.0.0",
         port=8080,
-        url_path=WEBHOOK_PATH,
-        webhook_url=WEBHOOK_URL
+        url_path="webhook"
     )
 
 if __name__ == "__main__":
